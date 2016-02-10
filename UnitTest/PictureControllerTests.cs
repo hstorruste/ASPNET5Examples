25e89f3b8461ffcb9f81573ec4135257795e7c20
s165519@stud.hioa.no <s165519@stud.hioa.no>
2016-02-10 07:17:41 +0100

    Lagt til testfiler.

---
+++ UnitTest/PictureControllerTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using ImageProcessorTest.Controllers;
+using UnitTest.Stubs;
+using BLL;
+using DAL;
+using Model;
+using System.Collections;
+
+namespace UnitTest
+{
+    public class PictureControllerTest
+    {
+        PictureController controller;
+
+        public PictureControllerTest()
+        {
+            controller = new PictureController(new PictureBLL(new DbPictureStub()));
+        }
+        [Fact]
+        public void Get_Pictures_Ok()
+        {
+            //Arrange
+            var expected = new List<Picture>()
+            {
+                new Picture
+                {
+                    id =1,
+                    url= "www.1.no",
+                    description = "Dette er en test!"
+                },
+                new Picture
+                {
+                    id =2,
+                    url= "www.2.no",
+                    description = "Dette er en test!"
+                }
+            };
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            //Assert.Equal(expected: expected, actual: result, comparer: new PictureListComparer());
+            Assert.Equal(expected[0].id, result[0].id);
+
+        }
+    }
+
+    //public class PictureListComparer : IEqualityComparer
+    //{
+    //    //public new bool Equals(object x, object y)
+    //    //{
+    //    //    var list1 = (Picture)x;
+    //    //    var list2 = (Picture)y;
+
+    //    //    //if (list1.Count != list2.Count)
+    //    //    //    return false;
+
+    //    //    //for(int i=0; i < list1.Count; ++i)
+    //    //    //{
+    //    //    if (list1.id != list2.id
+    //    //        || list1.url != list2.url
+    //    //        || list1.description != list2.description)
+    //    //        return false;
+    //    //    //}
+
+    //    //    return true;
+    //    //}
+
+    //    public int GetHashCode(object obj)
+    //    {
+    //        return obj.GetHashCode();
+    //    }
+    //}
+}
---
+++ UnitTest/Stubs/DbPictureStub.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Model;
+
+namespace UnitTest.Stubs
+{
+    public class DbPictureStub : IDbPictures
+    {
+        public Picture addPicture(Picture picture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Picture deletePicture(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Picture getPicture(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Picture> getPictureOfPage(int pageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Picture> getPictures()
+        {
+            return new List<Picture>()
+            {
+                new Picture
+                {
+                    id =1,
+                    url= "www.1.no",
+                    description = "Dette er en test!"
+                },
+                new Picture
+                {
+                    id =2,
+                    url= "www.2.no",
+                    description = "Dette er en test!"
+                }
+            };
+        }
+
+        public Picture updatePicture(Picture picture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
