40af993e7c5baab38496c6699429664e14586bc1
Hallbjørn Storruste <s165519@stud.hioa.no>
2016-03-31 15:32:54 +0200

    Lagt til ajax-kall for opplasting av bilder.

---
+++ ImageProcessorTest/src/ImageProcessorTest/gulpfile.js
@@ -0,0 +1,26 @@
+﻿/// <binding AfterBuild='moveToLibs' />
+
+var gulp = require('gulp');
+
+var paths = {
+    npmSrc: "./node_modules/",
+    libTarget: "./wwwroot/libs/"
+};
+
+var libsToMove = [
+    paths.npmSrc + '/angular2/bundles/angular2-polyfills.js',
+    paths.npmSrc + '/angular2/bundles/http.dev.js',
+    paths.npmSrc + '/angular2/bundles/router.js',
+    paths.npmSrc + '/systemjs/dist/system.js',
+    paths.npmSrc + '/systemjs/dist/system-polyfills.js',
+    paths.npmSrc + '/rxjs/bundles/Rx.js',
+    paths.npmSrc + '/angular2/bundles/angular2.dev.js',
+    paths.npmSrc + '/es6-shim/es6-shim.min.js',
+    paths.npmSrc + '/es6-shim/es6-shim.map',
+    paths.npmSrc + '/ng2-bootstrap/ng2-bootstrap.js',
+    paths.npmSrc + '/jquery/dist/jquery.min.js'
+];
+
+gulp.task('moveToLibs', function () {
+    return gulp.src(libsToMove).pipe(gulp.dest(paths.libTarget));
+});
